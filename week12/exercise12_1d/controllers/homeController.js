71db444db30523ef1a86e6bb6088e7cee4fa41c2
HD <155204068+hdvorani@users.noreply.github.com>
2025-11-29 08:26:14 -0500

    Create homeController.js

---
+++ week12/exercise12_1d/controllers/homeController.js
@@ -0,0 +1,10 @@
+// homeController.js
+"use strict";
+const Circle = require("../models/circle.js");
+
+exports.renderIndex = (req, res) => {
+
+    const radius = req.params.radius ? parseFloat(req.params.radius) : 1; // to make radius optional with default 1
+    const circle = new Circle(parseFloat(radius));
+    res.render("index", { circle: circle });
+};
