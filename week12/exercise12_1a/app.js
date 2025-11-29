27d9f9ece96a0230cb42812b545bf2516866111e
HD <155204068+hdvorani@users.noreply.github.com>
2025-11-29 08:19:08 -0500

    Create app.js

---
+++ week12/exercise12_1a/app.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+const homeController = require("./controllers/homeController");
+
+app.set("port", process.env.PORT || 3000);
+
+app.set("view engine", "ejs");
+
+app.get("/circle", homeController.renderIndex);
+
+app.listen(app.get("port"), () => {
+  console.log(`Server running on http://localhost:${app.get("port")}`);
+});
