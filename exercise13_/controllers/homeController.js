5233068084c2a319da948cbc4c993f4f7c99bbda
HD <155204068+hdvorani@users.noreply.github.com>
2025-12-05 08:46:52 -0500

    Delete exercise13_ directory

--- exercise13_/controllers/homeController.js
+++
@@ -1,8 +0,0 @@
-// controllers/homeController.js
-
-"use strict";
-const path = require("path");
-
-exports.renderIndex = (req, res) => {
-  res.sendFile(path.join(__dirname, '../public', 'index.html'));
-}
